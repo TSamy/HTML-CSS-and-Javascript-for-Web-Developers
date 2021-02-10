08356df33f9c87b8ba77910ff50ac8b2704be464
Samy <samjean005.com>
2021-02-10 13:37:33 -0500

    Assignment 5

--- Module5_solution/js/script.js
+++ Module5_solution/js/script.js
@@ -122,6 +122,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
 
+      homeHtmlToInsertIntoMainPage +=chosenCategoryShortName;
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
