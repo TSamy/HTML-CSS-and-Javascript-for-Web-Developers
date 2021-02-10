5256a3da0a1d3b4f72cf624e4aa2a571e4919faf
Samy <samjean005.com>
2021-02-10 12:55:18 -0500

    Assignment 5

--- Module5_solution/js/script.js
+++ Module5_solution/js/script.js
@@ -83,9 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function buildAndShowHomeHTML(responseText){
-  	document.querySelector("#main-content").innerHTML=responseText;
-  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -100,12 +98,16 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
 
-      // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+    	
+
+
+      var chosenCategoryShortName = chooseRandomCategory(categories);// TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
-
+      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
+      	var homeHtmlToInsertIntoMainPage =insertProperty( homeHtmlUrl,'randomCategoryShortName', chosenCategoryShortName);
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -124,6 +126,9 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
